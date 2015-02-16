fad4fbec1722977e29b8143c7f79f0426eb7eac3
Damien Doligez <damien.doligez-inria.fr>
2015-02-16 15:47:35 +0000

    cleanup; separate byte-code from native executables
    git-svn-id: http://caml.inria.fr/svn/ocaml/branches/new-test-framework@15842 f963ae5c-01c2-4b8c-9fe0-0dff7051ff02

--- testsuite/tests/backtrace/backtrace-common.sh
+++ testsuite/tests/backtrace/backtrace-common.sh
@@ -2,33 +2,14 @@
 #                                                                       #
 #                                 OCaml                                 #
 #                                                                       #
-#                 Xavier Clerc, SED, INRIA Rocquencourt                 #
+#                 Damien Doligez, Jane Street Capital                   #
 #                                                                       #
-#   Copyright 2010 Institut National de Recherche en Informatique et    #
+#   Copyright 2015 Institut National de Recherche en Informatique et    #
 #   en Automatique.  All rights reserved.  This file is distributed     #
 #   under the terms of the Q Public License version 1.0.                #
 #                                                                       #
 #########################################################################
 
-Tcompflags=-g
-Texec_env=OCAMLRUNPARAM="${OCAMLRUNPARAM},b=1"
-
-# Watch out, the order here is relevant:
-Ttests=Tbyte_compile              # First compile with byte-code
-Tmultiple Tbyte_run a b c d none  # Then run byte-code tests
-Ttests="$Ttests Topt_compile"     # Then compile with native code
-Tmultiple Topt_run a b c d none   # Then run native tests
-
-Targs_a=a
-
-Targs_b=b
-Texit_b=2
-
-Targs_c=c
-Texit_c=2
-
-Targs_d=d
-Texit_d=2
-
-Targs_none=
-Texit_none=2
+. Testcommon.sh
+Tml_files=backtrace.ml
+Ttests='Tbyte_compile_run Topt_compile_run'
